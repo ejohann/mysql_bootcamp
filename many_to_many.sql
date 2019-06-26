cbcd22f296719108c35473855cbedccfd935d2ec
Johanne <johanne_lewis@hotmail.com>
2019-06-26 10:54:39 +0100

    Comment out already used code

--- many_to_many.sql
+++ many_to_many.sql
@@ -86,8 +86,10 @@ SELECT title,
 */
 
 -- CHALLENGE TWO --
+/*
 SELECT title,
        AVG(rating) AS avg_rating
        FROM series
        INNER JOIN reviews
        ON series.id = reviews.series_id GROUP BY series.id ORDER BY avg_rating;
+*/
