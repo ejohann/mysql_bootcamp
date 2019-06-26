3f46d47fae24fa3cb1e653c1c2fd25211a1e8875
Johanne <johanne_lewis@hotmail.com>
2019-06-26 15:09:01 +0100

    Query for showing most popular day for user registrations

--- instagram_queries.sql
+++ instagram_queries.sql
@@ -6,3 +6,11 @@ SELECT username,
     FROM users
     ORDER BY created_at ASC LIMIT 5;  
 */
+
+-- what days of the week do most users register --
+
+SELECT 
+    COUNT(DAYOFWEEK(created_at)) AS registered_users,
+    DAYNAME(created_at) AS 'Day'
+    FROM users
+    GROUP BY DAYNAME(created_at) ORDER BY registered_users DESC;
