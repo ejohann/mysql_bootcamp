b8313f7186afeab61ee9de0a0c28a6c519285098
Johanne <johanne_lewis@hotmail.com>
2019-06-26 11:55:06 +0100

    Query using inner join to show average ratings rounded to two decimal places grouped by genre

--- many_to_many.sql
+++ many_to_many.sql
@@ -121,3 +121,9 @@ SELECT genre,
        ON series.id = reviews.series_id
        GROUP BY genre;
 
+SELECT genre,
+       ROUND(AVG(rating),2) AS avg_rating
+       FROM series
+       INNER JOIN reviews
+       ON series.id = reviews.series_id
+       GROUP BY genre;
