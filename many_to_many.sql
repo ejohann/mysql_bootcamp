8ff7ef6b9fb952113ae8c5f2bf6819288b165bcf
Johanne <johanne_lewis@hotmail.com>
2019-06-26 10:15:55 +0100

    Query to create reviewers table

--- many_to_many.sql
+++ many_to_many.sql
@@ -1,3 +1,10 @@
 -- CREATE DATABASE tv_review_app;
 
 -- USE tv_review_app;
+
+CREATE TABLE reviewers(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100)
+);
+
