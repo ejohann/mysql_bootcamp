e84019eca0c8b7bb737ce9e11b7edec184756561
Johanne <johanne_lewis@hotmail.com>
2019-06-26 13:46:43 +0100

    Fix bug in code

--- instagram_clone.sql
+++ instagram_clone.sql
@@ -11,8 +11,8 @@ CREATE TABLE photos(
     id INT AUTO_INCREMENT PRIMARY KEY,
     image_url VARCHAR(255) NOT NULL,
     user_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
-    FOREIGN KEY(user_id) REFERENCES users.id
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
 CREATE TABLE comments(
@@ -20,7 +20,7 @@ CREATE TABLE comments(
     comment_text VARCHAR(255) NOT NULL,
     user_id INT NOT NULL,
     photo_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
-    FOREIGN KEY(user_id) REFERENCES users.id
-    FOREIGN KEY(photo_id) REFERENCES photos.id
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id)
 );
