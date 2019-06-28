6b10d13779a9e03cfd07f93f099b47546838e044
Johanne <johanne_lewis@hotmail.com>
2019-06-28 20:07:15 +0100

    Fix bug in code

--- triggers.sql
+++ triggers.sql
@@ -28,10 +28,10 @@ DELIMITER $$
 CREATE TRIGGER must_be_adult
     BEFORE INSERT ON users FOR EACH ROW
     BEGIN
-        IF NEW.age 18
+        IF NEW.age < 18
         THEN
             SIGNAL SQLSTATE '45000'
-                SET MESSAGE_TEXT = 'Must be an adult';
+                SET MESSAGE_TEXT = 'Must be an adult!';
         END IF;
     END;
 $$
