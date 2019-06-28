0e7d6fdb9ff44b459706c03e84f694869b00b0a9
Johanne <johanne_lewis@hotmail.com>
2019-06-28 19:55:35 +0100

    Create database and user table

--- triggers.sql
+++ triggers.sql
@@ -1,5 +1,37 @@
+CREATE DATABASE trigger_demo;
+
+USE trigger_demo;
+
+CREATE TABLE users(
+        username VARCHAR(100),
+        age INT
+);
+
+
+/*
+
 CREATE TRIGGER trigger_name
         trigger_time trigger_event ON table_name FOR EACH ROW
         BEGIN
         
         END;
+        
+
+
+
+DELIMITER $$
+
+CREATE TRIGGER must_be_adult
+    BEFORE INSERT ON people FOR EACH ROW
+    BEGIN
+        IF NEW.age 18
+        THEN
+            SIGNAL SQLSTATE '45000'
+                SET MESSAGE_TEXT = 'Must be an adult';
+        END IF;
+    END;
+$$
+
+DELIMITER;
+
+*/
